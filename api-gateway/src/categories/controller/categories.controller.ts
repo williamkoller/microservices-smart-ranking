9b66f4a6752fe17d282053aa00197771e57634e3
william koller <wkoller25@gmail.com>
2020-12-31 19:11:07 -0300

    feat: add updateCategory

--- api-gateway/src/categories/controller/categories.controller.ts
+++ api-gateway/src/categories/controller/categories.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common'
+import { Body, Controller, Get, Logger, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common'
+import { Observable } from 'rxjs'
 import { ClientProxyProvider } from 'src/shared/providers/client-proxy.provider'
 import { CreateCategoryDto } from '../dtos/create-category.dto'
+import { UpdateCategoryDto } from '../dtos/update-category.dto'
 
 @Controller('api/v1/categories')
 export class CategoriesController {
@@ -10,7 +12,19 @@ export class CategoriesController {
 
   @Post()
   @UsePipes(ValidationPipe)
-  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
-    this.clientProxyProvider.getAdminServerInstance().emit('create-category', createCategoryDto)
+  createCategory(@Body() createCategoryDto: CreateCategoryDto): Observable<any> {
+    return this.clientProxyProvider.getAdminServerInstance().emit('create-category', createCategoryDto)
+  }
+  @Get()
+  searchAllCategories(@Query('categoryId') _id: string): Observable<any> {
+    return this.clientProxyProvider.getAdminServerInstance().send('search-all-categories', _id ? _id : '')
+  }
+
+  @Put('categories/:_id')
+  @UsePipes(ValidationPipe)
+  updateCategory(@Body() updateCategoryDto: UpdateCategoryDto, @Param() _id: string): Observable<any> {
+    return this.clientProxyProvider
+      .getAdminServerInstance()
+      .emit('update-category', { id: _id, category: updateCategoryDto })
   }
 }
