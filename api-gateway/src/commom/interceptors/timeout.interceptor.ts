c3c9102eabc849d628f35f1a37f03a511e694fe3
william koller <wkoller25@gmail.com>
2020-12-31 19:10:43 -0300

    feat: add Interceptors

---
+++ api-gateway/src/commom/interceptors/timeout.interceptor.ts
@@ -0,0 +1,10 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { timeout } from 'rxjs/operators'
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(timeout(5000))
+  }
+}
