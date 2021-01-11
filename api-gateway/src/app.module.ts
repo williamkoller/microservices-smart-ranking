e2a5ce589a9db812805cd35ac8a745ddef08c824
william koller <wkoller25@gmail.com>
2021-01-11 19:33:19 -0300

    feat: add PlayersModule

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CategoriesModule } from './categories/categories.module'
+import { PlayersModule } from './players/players.module'
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { CategoriesModule } from './categories/categories.module'
       isGlobal: true,
     }),
     CategoriesModule,
+    PlayersModule,
   ],
   controllers: [],
   providers: [],
