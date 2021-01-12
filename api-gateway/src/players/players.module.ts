3a5fd524712eca3d4dc61e6c40331ce85f5688f8
william koller <wkoller25@gmail.com>
2021-01-11 21:55:27 -0300

    feat: set ClientProxyModule

--- api-gateway/src/players/players.module.ts
+++ api-gateway/src/players/players.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { ClientProxyModule } from 'src/shared/modules/client-proxy.module'
 import { PlayersController } from './controller/players.controller'
 
 @Module({
+  imports: [ClientProxyModule],
   controllers: [PlayersController],
 })
 export class PlayersModule {}
