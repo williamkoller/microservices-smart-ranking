45243c43747fa07d0020953432eded60799137d6
william koller <wkoller25@gmail.com>
2021-02-27 17:34:06 -0300

    feat: organize files disuse

--- micro-challenges/src/main.ts
+++ micro-challenges/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT, () => logger.log('Challenges'));
 }
 bootstrap();
