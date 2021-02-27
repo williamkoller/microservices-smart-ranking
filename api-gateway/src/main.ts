8410b05fccaee20f0d8e56c5c6acb0501d13093d
william koller <wkoller25@gmail.com>
2021-02-27 17:42:17 -0300

    feat: add NestExpressApplication

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -4,11 +4,12 @@ import { HttpExceptionFilter } from '@/common/filters/http-exception.filter'
 import { LoggingInterceptor } from '@/common/interceptors/logging.interceptor'
 import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor'
 import { Logger } from '@nestjs/common'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 const logger = new Logger('Main')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.setGlobalPrefix('api/v1')
   app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
