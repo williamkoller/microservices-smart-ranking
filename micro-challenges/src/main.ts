8b1f358c6fb5a545fbd925449073711cfd0ab566
william koller <wkoller25@gmail.com>
2021-05-17 22:41:08 -0300

    feat: adjusted code in micro-challenges

--- micro-challenges/src/main.ts
+++ micro-challenges/src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@/app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '@/app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { Logger } from '@nestjs/common'
 
-const logger = new Logger('Main');
+const logger = new Logger('Main')
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT, () => logger.log('Challenges'));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  await app.listen(process.env.PORT, () => logger.log('Challenges'))
 }
-bootstrap();
+bootstrap()
